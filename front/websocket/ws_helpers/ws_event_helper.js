c4a9d96967c95513104f34117978320b67bac03b
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-08 05:48:19 +0100

    OnError

--- front/websocket/ws_helpers/ws_event_helper.js
+++ front/websocket/ws_helpers/ws_event_helper.js
@@ -5,7 +5,7 @@ import { onUpgradeUnlock } from "../../game/on_upgrade_unlock.js";
 import { onConnectionEvent } from "./connection_event_handler.js";
 
 const opEvents = [
-    { "op": 0, "function": onError }
+    { "op": 0, "function": onError },
     { "op": 1, "function": onConnectionEvent },
     { "op": 2, "function": onScore},
     { "op": 3, "function": onUpgrade },
