2849bf34534e61d935f9e5406cc1fd5c49daaa0e
Thomas <billequin.thomas@gmail.com>
2023-12-08 05:54:38 +0100

    qslidggkjesg la poulele

--- front/index.js
+++ front/index.js
@@ -1,11 +1,16 @@
-import { onScore } from "./game/on_score.js";
 import { startWebSocket } from "./websocket/websocket_client.js";
 
 window.addEventListener('load', startWebSocket);
 
+document.addEventListener('keydown', function(event) {
+    console.log("event", event.key);
+    if (event.key === ' ' || event.code === 'Space') {
+        document.location.href="./egg.html"; 
+    }
+});    
+
 export function updateBar(Year) {
     let progress =  (Year - 1882) / (2100 - 1882) * 100;
-    console.log("progress", progress);
     document.getElementById("progress-bar").style.width = `${progress}%`;
     updatePlanet(Year)
 }
@@ -18,3 +23,4 @@ function updatePlanet(year){
         document.getElementById("terre").src = "ressources/terre_3.png"
     }
 }
+
