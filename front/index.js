f0abc51986520b3b82f628486fed8f5ccbbd5fef
LeBihanLeo <70631802+LeBihanLeo@users.noreply.github.com>
2023-12-08 04:51:47 +0100

    normalement les planete change

--- front/index.js
+++ front/index.js
@@ -7,5 +7,14 @@ export function updateBar(Year) {
     let progress =  (Year - 1882) / (2100 - 1882) * 100;
     console.log("progress", progress);
     document.getElementById("progress-bar").style.width = `${progress}%`;
+    updatePlanet(Year)
+}
 
+function updatePlanet(year){
+    if(year == 2000){
+        document.getElementById("terre").src = "ressources/terre_2.png"
+    }
+    else if(year == 2100){
+        document.getElementById("terre").src = "ressources/terre_3.png"
+    }
 }
